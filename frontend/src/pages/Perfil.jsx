db77d6724f4a03a9d53de0d5c78a343c89f04276
francogcn <francogcn@hotmail.com>
2024-10-30 22:08:53 -0300

    Perfil.jsx

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -5,7 +5,23 @@ export default function Perfil() {
     <div className="container">
     <div className="profile-banner"></div>
       <div className="perfil">
-        <h1>Perfil</h1>
+        <div className="profile-pic"></div>
+        <h1>Username</h1>
+        <div className="row">
+          <div className="col-sm-4 perfil-info">
+            20
+            <span className="perfil-info-bubtitle">Leídos</span>
+          </div>
+          <div className="col-sm-4 perfil-info">
+            15
+            <span className="perfil-info-bubtitle">Reseñas</span>
+            </div>
+          <div className="col-sm-4 perfil-info">
+            3
+            <span className="perfil-info-bubtitle">Amigos</span>
+            </div>
+        </div>
+        <button type="button" className="btn btn-info">Agregar</button>
       </div>
     </div>
     </>
