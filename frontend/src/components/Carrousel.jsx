35374e1373e206d196a53c7532cb482499120674
francogcn <francogcn@hotmail.com>
2024-11-07 20:42:16 -0300

    Update Carrousel.jsx

--- frontend/src/components/Carrousel.jsx
+++ frontend/src/components/Carrousel.jsx
@@ -3,7 +3,7 @@ import NewRelease from "./NewRelease";
 export default function Carrousel() {
     return (
         <>
-            <div id="nuevosLibros" className="carousel slide">
+            <div id="nuevosLibros" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#nuevosLibros" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#nuevosLibros" data-bs-slide-to="1" aria-label="Slide 2"></button>
