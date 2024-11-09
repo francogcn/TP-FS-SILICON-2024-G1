8faa2c75bce851236fd088cc2e9517694ac65061
francogcn <francogcn@hotmail.com>
2024-11-09 03:11:53 -0300

    Update reviewCard.jsx
    Toma del backend las reseñas de amigos y muestra en el inicio

--- frontend/src/components/reviewCard.jsx
+++ frontend/src/components/reviewCard.jsx
@@ -1,32 +1,42 @@
 import { NavLink } from "react-router-dom"
 import { useState, useEffect } from "react"
 
-export default function ReviewCard({titulo, resenia, puntaje}) {
+export default function ReviewCard() {
     const id_usuario = 1; //Esto se debe automatizar
-    const[reviewAmigos, setReviewAmigos] = useState([]);
-    useEffect(()=>{
-        const fetchReseniaAmigos = async()=>{
-            try{
+    const [reviewAmigos, setReviewAmigos] = useState([]);
+
+    useEffect(() => {
+        const fetchReseniaAmigos = async () => {
+            try {
                 const response = await fetch(`http://localhost:8080/api/amistad/resenias_amigos/${id_usuario}`);
                 const data = await response.json();
                 setReviewAmigos(data);
 
-            } catch(error){
+            } catch (error) {
                 console.error("Error al obtener los préstamos:", error)
             }
         };
         fetchReseniaAmigos();
-    },[]);
+    }, []);
 
     return (
         <>
-            <div className="card text-center mb-3" style="width: 18rem;">
-                <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <NavLink href="/" className="btn btn-primary">Go somewhere</NavLink>
-                </div>
+            <div className="row resenias-amigos-home">
+
+                {reviewAmigos.map(review => (
+                    <div key={review.id_resenia} className="col-sm-6 col-md-4">
+                        <div className="card text-center mb-3">
+                            <div className="card-body">
+                                <h5 className="card-title">{review.titulo_libro}</h5>
+                                <p className="card-text">{review.texto_resenia}</p>
+                                <p className="card-text">
+                                    <span className="badge badge text-bg-light">{review.nombre_amigo +" "+ review.apellido_amigo}</span>
+                                </p>
+                                <NavLink href="/" className="btn btn-primary">Go somewhere</NavLink>
+                            </div>
+                        </div></div>))}
             </div>
+
         </>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Carrousel from "../components/Carrousel";
+import ReviewCard from "../components/ReviewCard";
 
 export default function Home(){
     
@@ -6,6 +7,7 @@ export default function Home(){
         <>
             <div className="container">
                 <Carrousel/>
+                <ReviewCard/>
             </div>
         </>
     )
